a3499f7e5da1d4e5a55ce84b39db675411ad68aa
nyccowgirl <nyc_cowgirl@hotmail.com>
2020-10-29 01:39:56 -0500

    Initial commit

---
+++ src/com/company/Asterisks.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+// Trang Hoang
+
+public class Asterisks {
+
+    public static void main(String[] args) {
+        int count;
+        char goAgain = 'y';
+        Scanner input = new Scanner(System.in);
+
+        while (goAgain != 'n') {
+            System.out.print("How many asterisks?: ");
+            count = input.nextInt();
+
+            for (int i = 0; i < count; i++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+            System.out.print("Go again? (y/n): ");
+
+            goAgain = input.next().charAt(0);
+        }
+    }
+}
